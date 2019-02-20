5c97ebfd8a7a90ff50ff7d557c6d1404b389d2a8
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-02-20 10:10:28 +0000

    [EN-89] Created methods for the index page in order to navigate to the student and admin login pages

--- features/lib/pages/index.rb
+++ features/lib/pages/index.rb
@@ -3,4 +3,15 @@ require 'capybara/dsl'
 class Index
   include Capybara::DSL
   
+  ADMIN_NAME_BTN = 'Admin'
+  STUDENT_NAME_BTN = 'Student'
+
+  def click_admin
+    click_button(ADMIN_NAME_BTN)
+  end
+
+  def click_student
+    click_button(STUDENT_NAME_BTN)
+  end
+  
 end
