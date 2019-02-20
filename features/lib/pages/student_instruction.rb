35bec63d728ec4a81ce811fc8423a1c9aad879c5
Shah <skhan@spartaglobal.com>
2019-02-20 10:36:00 +0000

    [EN-96] Fixed the env file and put in the methods for the student instruction page

--- features/lib/pages/student_instruction.rb
+++ features/lib/pages/student_instruction.rb
@@ -3,4 +3,11 @@ require 'capybara/dsl'
 class StudentInstruction
   include Capybara::DSL
   
+  H1 = 'HOW TO USE'
+  ADMIN_INFO_H2 = 'Admin Info'
+  STUDENT_INFO_H2 = 'Student Info'
+  HOW_TO_SAVE_H2 = 'How to Save'
+  BACK_BUTTON = 'Back'
+
+  
 end
