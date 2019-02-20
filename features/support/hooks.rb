4125f594eb4d78311b2c23da14b1c59f6f7dacbf
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-02-20 12:20:44 +0000

    [EN-89] Added .env file and hid the email and password from github

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,7 @@
 Before do
+  Dotenv.load('.env')
   @sql_automated_checker = SqlAutomatedCheckerTesting.new
+  @email = ENV['EMAIL']
+  @password = ENV['PASSWORD']
   @sql_automated_checker.index.visit_index
 end
