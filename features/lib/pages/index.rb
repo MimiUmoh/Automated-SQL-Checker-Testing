221eab0253ad39e2f5ccf3faffcbad41d479c6b6
Tech-A45 <tech-a45@Tech-A45xs-MacBook-Air.local>
2019-02-20 10:10:36 +0000

    created methods for the index page in order to retrieve admin and student buttons as well as finding the header on the page

--- features/lib/pages/index.rb
+++ features/lib/pages/index.rb
@@ -2,5 +2,21 @@ require 'capybara/dsl'
 
 class Index
   include Capybara::DSL
+
+  ADMIN_NAME_BTN = 'Admin'
+  STUDENT_NAME_BTN = 'Student'
+  SQL_ASSESSMENT_HEADER = 'h1'
+
+  def click_admin
+    click_button(ADMIN_NAME_BTN)
+  end
+
+  def click_student
+    click_button(STUDENT_NAME_BTN)
+  end
+
+  def find_header
+    find(:css, SQL_ASSESSMENT_HEADER).text
+  end
   
 end
